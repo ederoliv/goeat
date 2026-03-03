dd5185ca542558defdd881d32ea6e1c05480ebec
Eder Oliveira <ederoliveira152@gmail.com>
2026-03-03 18:40:26 -0300

    adding browser tab icon

--- src/routes/menu.tsx
+++ src/routes/menu.tsx
@@ -2,5 +2,5 @@ import { createFileRoute } from '@tanstack/react-router'
 import { MenuPage } from '../pages/MenuPage'
 
 export const Route = createFileRoute('/menu')({
-  component: MenuPage,
+  component: MenuPage
 })
